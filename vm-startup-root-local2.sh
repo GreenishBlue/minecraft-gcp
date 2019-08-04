3db7108334f0817167fb004902da4791d128a785
Cameron Brown <hello@cameronbrown.co.uk>
2019-08-04 22:17:08 +0100

    fix apt-get syntax error

--- vm-startup-root-local2.sh
+++ vm-startup-root-local2.sh
@@ -7,10 +7,10 @@ SERVER_PATH=/opt/minecraft-server
 WORLD_PATH=${SERVER_PATH}/world
 
 echo 'Updating system..'
-apt-get get update
-apt-get get upgrade
+apt-get update
+apt-get upgrade
 echo 'Installing tools'
-apt-get get install git screen java
+apt-get install git screen java
 
 cat /etc/passwd | grep ${USER_NAME} >/dev/null 2>&1
 if [ $? -eq 0 ] ; then
